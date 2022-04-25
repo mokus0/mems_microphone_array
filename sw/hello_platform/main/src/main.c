559799d4b292b95e0412c260c46de8b0d77dcf29
James Cook <mokus@deepbondi.net>
2022-04-24 18:57:32 -0700

    add DDR3 interface

--- sw/hello_platform/main/src/main.c
+++ sw/hello_platform/main/src/main.c
@@ -5,11 +5,19 @@
 
 #define LED_DELAY 10000000
 
+uint32_t *ddr3 = (void*) 0x80000000;
+unsigned const ddr3_words = 256 * 1024 * 1024 / sizeof(*ddr3);
+
 int main() {
     XGpio_Config *cfg_ptr;
     XGpio led_device;
 
-    xil_printf("Booted microblaze...\r\n");
+    xil_printf("Initializing DDR3");
+    for (unsigned i = 0; i < ddr3_words; i++) {
+        ddr3[i] = i;
+        if ((i % (1024 * 1024 / sizeof(*ddr3))) == 0) xil_printf(".");
+    }
+    xil_printf("done.\r\n");
 
     cfg_ptr = XGpio_LookupConfig(XPAR_AXI_GPIO_0_DEVICE_ID);
     XGpio_CfgInitialize(&led_device, cfg_ptr, cfg_ptr->BaseAddress);
@@ -20,7 +28,7 @@ int main() {
     while (1) {
         x++;
 
-        xil_printf("Tick %u\r\n", x);
+        xil_printf("Tick %u: %u\r\n", x, ddr3[x]);
         XGpio_DiscreteWrite(&led_device, 1, x);
         for (volatile int delay = 0; delay < LED_DELAY; delay++);
     }
